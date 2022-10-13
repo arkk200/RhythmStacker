40999d5df85c98236b2cdb3c899680254d07d14f
arkk200 <arkk20000@gmail.com>
2022-10-13 22:39:27 +0900

    클릭 이벤트에 stkArr.shift() 구현(미완)

--- src/components/Stack/Stacks.js
+++ src/components/Stack/Stacks.js
@@ -2,18 +2,6 @@ import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef, useState } from "react";
 import Stack from "./Stack/Stack";
 
-const stkArr = [
-    {
-        pos: {
-            x: 0,
-            z: 0
-        },
-        wid: 3,
-        dep: 3,
-        dir: null
-    }
-];
-
 const stkdArr = [];
 
 let time = 0;
@@ -23,28 +11,58 @@ let prevDir = null;
 const speed = 10;
 
 function Stacks() {
+    const [stkArr, setStkArr] = useState([
+        {
+            pos: {
+                x: 0,
+                z: 0
+            },
+            wid: 3,
+            dep: 3,
+            dir: null
+        }
+    ]);
     const stksRef = useRef([]);
     const [rendState, setRendState] = useState(false);
 
     useEffect(() => {
         window.addEventListener('click', () => {
-            console.log(stkArr);
-            stkArr.map((stk, i) => {
-                console.log (stksRef.current, i);
-                return {
-                    pos: stksRef.current[i].position,
-                    wid: stk.wid,
-                    dep: stk.dep,
-                    dir: stk.dir
+            // stkArr.map((stk, i) => {
+            //     // console.log (stksRef.current, i);
+            //     return {
+            //         pos: stksRef.current[i].position,
+            //         wid: stk.wid,
+            //         dep: stk.dep,
+            //         dir: stk.dir
+            //     }
+            // });
+            setStkArr(
+                cur => {
+                    return cur.map((stk, i) => {
+                        // console.log (stksRef.current, i);
+                        return {
+                            pos: stksRef.current[i].position,
+                            wid: stk.wid,
+                            dep: stk.dep,
+                            dir: stk.dir
+                        }
+                    })
                 }
-            });
-            stkArr.shift();
+            )
+            // stkArr.shift();
+            // stkdArr.push(stksRef.current);
+            console.log(123123123);
+            // stksRef.current.shift();
+            // setStkArr(stkArr.slice(1));
+            // console.log(stkArr);
+            // console.log
+            // setRendState(c => !c);
         })
     })
 
     useFrame((_, dt) => {
         stksRef.current.forEach((stkRef, i) => {
-            // console.log(prevDir)
+            // console.log(i);
             stkRef.position.x += stkArr[i].dir === 'x' ? speed * dt : 0;
             stkRef.position.z += stkArr[i].dir === 'z' ? speed * dt : 0;
         })
@@ -52,14 +70,25 @@ function Stacks() {
         if(time >= 60/bpm) {
             time -= 60/bpm;
             prevDir = stkArr.at(-1).dir;
-            stkArr.push({
-                ...stkArr.at(-1),
-                pos : {
-                    x: prevDir === 'z' ? -10 : 0,
-                    z: prevDir === 'z' ? 0 : -10
-                }, 
-                dir: prevDir === 'z' ? 'x' : 'z'
-            });
+            // stkArr.push({
+            //     ...stkArr.at(-1),
+            //     pos : {
+            //         x: prevDir === 'z' ? -10 : 0,
+            //         z: prevDir === 'z' ? 0 : -10
+            //     }, 
+            //     dir: prevDir === 'z' ? 'x' : 'z'
+            // });
+            setStkArr([
+                ...stkArr,
+                {
+                    ...stkArr.at(-1),
+                    pos : {
+                        x: prevDir === 'z' ? -10 : 0,
+                        z: prevDir === 'z' ? 0 : -10
+                    }, 
+                    dir: prevDir === 'z' ? 'x' : 'z'
+                }
+            ])
             console.log(stkArr.length, prevDir);
             setRendState(c => !c);
         }
@@ -75,7 +104,11 @@ function Stacks() {
             />
         ))}
         {stkdArr.map((stkd, i) => (
-            <Stack key={i} />
+            <Stack 
+                key={i} 
+                position={[stkd.pos.x, i, stkd.pos.z]} 
+                args={[stkd.wid, 1, stkd.dep]}
+            />
         ))}
         </>
     );
